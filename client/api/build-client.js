f29eebad6382a43d55fd79223f1fb4228393557d
vksawe <vskiprotich@gmail.com>
2021-03-20 01:28:11 +0300

    changes to client baseUrL TO PROD

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,8 +3,8 @@ import axios from "axios";
 export default ({ req }) => {
   if (typeof window === "undefined") {
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "www.vksawe-ticketing-app.xyz",
+      //"http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
 
       headers: req.headers,
     });
